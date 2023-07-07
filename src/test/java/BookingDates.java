aadf7e9ba6d463ca99b9be6266f10e0c99153438
olemykha <alex.mkhlk@gmail.com>
2023-07-07 11:44:17 +0000

    add homework16

---
+++ src/test/java/BookingDates.java
@@ -0,0 +1,14 @@
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class BookingDates {
+    private LocalDate checkin;
+    private LocalDate checkout;
+
+}
---
+++ src/test/java/ResponseBookingId.java
@@ -0,0 +1,14 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class ResponseBookingId {
+    private Integer bookingid;
+
+}
